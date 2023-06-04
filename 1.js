7c3731530fad87414760aff0c107c0ae7580f7cb
ngovanquoc4802000 <ngovanquoc480@gmail.com>
2023-06-04 16:05:33 +0700

    update

---
+++ 1.js
@@ -0,0 +1,169 @@
+<div class="row margin-top">
+<div class="section-hd col-md-3">
+    <div class="section-sidebar ">
+
+        <h5 class="section-heading">Lastest News</h5>
+        <hr>
+        <div class="link-loom">
+            <a href="" class="lastest-new">
+                <img src="./imageProject/timthumb.jpg" alt="Introduce" class="lastest">
+                <p>JA91-190 air jet loom</p>
+            </a>
+        </div>
+        <h5 class="section-heading">Product List</h5>
+        <hr>
+        <ul class="section-list">
+            <li>
+                <a href="" class="section-item" title="Introduce">
+                    <i class="fas fa-user-alt"></i>
+                    Introduce
+                </a>
+            </li>
+            <li>
+                <a href="" class="section-item" title="Loom Knitting">
+                    <i class="fas fa-radiation-alt"></i>
+                    Loom-Knitting
+                </a>
+            </li>
+            <li>
+                <a href="" class="section-item" title="Dyeing machine">
+                    <i class="fas fa-truck-pickup"></i>
+                    Dyeing machine
+                </a>
+            </li>
+            <li>
+                <a href="" class="section-item" title="Printing-machine">
+                    <i class="fas fa-archive"></i>
+                    Printing-machine
+                </a>
+            </li>
+            <li>
+                <a href="" class="section-item" title="Stenter">
+                    <i class="fas fa-car-side"></i>
+                    Stenter
+                </a>
+            </li>
+            <li>
+                <a href="" class="section-item" title="Finishing machine">
+                    <i class="fas fa-rainbow"></i>
+                    Finishing machine
+                </a>
+            </li>
+            <li>
+                <a href="" class="section-item" title="Other machine">
+                    <i class="fas fa-file-invoice"></i>
+                    Other machine
+                </a>
+            </li>
+            <li>
+                <a href="" class="section-item">
+                    <i class="fas fa-bells"></i>
+                    Support
+                </a>
+            </li>
+        </ul>
+        <h5 class="section-heading">Statistics</h5>
+        <hr>
+        <p>
+            <span>User Accessed</span>
+            <span>1,212,017</span>
+        </p>
+        <p>
+            <span>User Online</span>
+            <span>3</span>
+        </p>
+        <h5 class="section-heading">Finds us on...</h5>
+        <hr>
+    </div>
+</div>
+{/* header */}
+<header class="header ">
+            <div class="list-header">
+                <img src="./imageProject/logo.jpg" alt="icon thẻ hàng" class="img-header">
+                <ul class="list-producte">
+                    <li>
+                        <a href="" class="header-link">Home</a>
+                    </li>
+                    <li>
+                        <a href="" class="header-link">Products
+                            <i class="fas fa-sort-down" class="header-icon"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" class="header-link">Introduce</a>
+                    </li>
+                    <li>
+                        <a href="" class="header-link">News
+                            <i class="fas fa-sort-down" class="header-icon"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" class="header-link">Video</a>
+                    </li>
+                    <li>
+                        <a href="" class="header-link">Contact us</a>
+                    </li>
+                    <img src="" alt="" class="img-USA">
+                </ul>
+            </div>
+            <form action="" class="form-header">
+                <div class="form">
+                    <input type="text" autocomplete="off" class="input-header">
+                    <i class="fas fa-search" class="icon-header"></i>
+                    <i class="fas fa-bars translating"></i>
+                </div>
+                <div class="product">
+                    <p><a href="" class="product-link">[0] Sản Phẩm
+                        </a>
+    
+                    </p>
+                    <img src="./imageProject/cart.png" alt="the cart" class="product-cart">
+                </div>
+            </form>
+    
+        </header>
+        
+        <div class="footer-About">
+                    <article>
+                        <h2>ABOUT US</h2>
+                        <p><strong>NCSC Vietnam</strong> - operation in the fields of Commerce - Engineering & Services.
+                            Specialist suppliers of machinery, industrial equipment, textile (new machine & Used).</p>
+                        <p> <strong>ThietbiDetNhuom.com</strong> - members of ncsc.com.vn page. A address information exchange,
+                            experience in manufacturing business of Weaving - Printing - Dyeing.</p>
+                    </article>
+    
+                    <article>
+                        <h2>INFORMATION - POLICY</h2>
+                        <a href="">
+                            Consultants
+                        </a>
+                        <a href="">
+                            Regulation purchase
+                        </a>
+                        <a href="">
+                            How to order
+                        </a>
+                        <a href="">
+                            Payment instructions
+                        </a>
+                        <a href="">
+                            Shipping
+                        </a>
+                        <a href="">
+                            Delivery
+                        </a>
+                        <a href="">
+                            Information Security Policy
+                        </a>
+                        <a href="">
+                            Customer opinions
+                        </a>
+                    </article>
+                    <article>
+                        <h2>CONTACT US</h2>
+                        <p>Add: 268/26 Nguyen thai Binh St,.Ward 12 , Tan binh Dist., HCMC - VN</p>
+                        <p>Hotline :<strong>(+84)  376 422 968</strong></p>
+                        <p>Email: thietbidetnhuom@gmail.com</p>
+                        <a href="https://www.facebook.com/Thietbidetnhuom/?ref=profile">Facebook.com/Cty TNHH NCS Viet nam</a>
+                    </article>
+                </div>
